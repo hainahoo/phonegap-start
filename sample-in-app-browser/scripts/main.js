f8d9b743642a7a0c81e5eb0495839837e3e9da62
hainahoo <nagendran.p@rediffmail.com>
2013-07-08 14:47:15 +0530

    Update main.js

--- sample-in-app-browser/scripts/main.js
+++ sample-in-app-browser/scripts/main.js
@@ -18,15 +18,7 @@ Application.prototype = {
 		var that = this,
 		openLocal = document.getElementById("openLocal"),
 		openExternalInAppBrowser = document.getElementById("openExternalInAppBrowser"),
-		openPDF = document.getElementById("openPDF"),
-		openExternalPDF = document.getElementById("openExternalPDF"),
-		openExternalInSystemBrowser = document.getElementById("openExternalInSystemBrowser");
-        
-		openLocal.addEventListener("click", that.openLocal);		
-		openExternalInAppBrowser.addEventListener("click", that.openExternalInAppBrowser);
-		openPDF.addEventListener("click", that.openPDF);
-		openExternalPDF.addEventListener("click", that.openExternalPDF);
-		openExternalInSystemBrowser.addEventListener("click", that.openExternalInSystemBrowser);
+		
 	},
     
 	openLocal: function() {
@@ -36,34 +28,4 @@ Application.prototype = {
 	openExternalInAppBrowser:  function () {
 		window.open("http://172.16.1.101/emcsfa", "_blank","location=no");
 	},
-    
-	openExternalPDF:  function () {
-		if (device.uuid == "e0101010d38bde8e6740011221af335301010333" || device.uuid == "e0908060g38bde8e6740011221af335301010333") {
-			alert("Not Supported in Simulator.");
-		}
-		else {
-			if (device.platform == 'Android') {
-				window.open("http://samplepdf.com/sample.pdf", "_system");
-			}
-			else
-				window.open("http://samplepdf.com/sample.pdf", "_blank");
-		}
-	},
-	
-	openPDF: function() {
-		if (device.uuid == "e0101010d38bde8e6740011221af335301010333" || device.uuid == "e0908060g38bde8e6740011221af335301010333") {
-			alert("Not Supported in Simulator.");
-		}
-		else {
-			if (device.platform == 'Android') {
-				window.open("whitepaper-mobile-developer-guidance.pdf", "_system");
-			}
-			else
-				window.open("whitepaper-mobile-developer-guidance.pdf", "_blank");
-		}
-	},
-    
-	openExternalInSystemBrowser:function () {
-		window.open("http://wiki.apache.org/cordova/InAppBrowser", "_system");
-	}
 }
